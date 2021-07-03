644222ccde2786bad0e17d0a6a9d1204fcfc24c6
tobby811023 <d1004221039@gmail.com>
2021-07-03 11:22:14 +0800

    add change owl

--- 4/js/owl.js
+++ 4/js/owl.js
@@ -2,8 +2,8 @@ $('.owl-carousel').owlCarousel({
     loop: true,
     margin:20,
     padding:20,
-    // autoplay: true,
-    // autoplayTimeout: 5000,
+    autoplay: true,
+    autoplayTimeout: 5000,
     responsive:{
         0:{
             items:1
